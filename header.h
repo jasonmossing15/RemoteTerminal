9745780d7dd2457003c1095bb007cc592e4497a2
Jason Mossing <c15jason.mossing@usafa.edu>
2014-04-03 22:34:01 -0600

    working switch functionality in microblaze, next step is to figure out how to interface the vga driver.

--- header.h
+++ header.h
@@ -8,13 +8,18 @@
 #ifndef HEADER_H_
 #define HEADER_H_
 
-unsigned char space = 0x20;
-unsigned char NewLine = 0x0A;
-unsigned char CR = 0x0D;
+
 
 unsigned char outputUpperled(unsigned char c);
 unsigned char outputLowerled(unsigned char c);
 int checkLED(unsigned char c1, unsigned char c2, unsigned char c3);
+int checkSWT(unsigned char c1, unsigned char c2, unsigned char c3);
+int checkVGA(unsigned char c1, unsigned char c2, unsigned char c3);
+unsigned char getTopNibble(unsigned char ascii);
+unsigned char getBtmNibble(unsigned char ascii);
+unsigned char topNibbleToAscii(unsigned char nibble);
+unsigned char btmNibbleToAscii(unsigned char nibble);
+void outputSWT(unsigned char ascii);
 unsigned char getLEDVal();
 
 
--- main.c
+++ main.c
@@ -8,7 +8,11 @@
 #include <xuartlite_l.h>
 #include <xparameters.h>
 #include <xil_io.h>
-#include <header.h>
+#include "header.h"
+
+unsigned char space = 0x20;
+unsigned char NewLine = 0x0A;
+unsigned char CR = 0x0D;
 
 int main(void){
 	while(1){
@@ -23,8 +27,19 @@ int main(void){
 		XUartLite_SendByte(0x84000000, c3);
 
 		if(checkLED(c1, c2, c3)){
+			XUartLite_SendByte(0x84000000, space);
 			unsigned char led = getLEDVal();
 			Xil_Out8(0x83000000, led);
+		}
+		if(checkSWT(c1, c2, c3)){
+			XUartLite_SendByte(0x84000000, space);
+			unsigned char swt = Xil_In8(0x82000000);
+			outputSWT(swt);
+		}
+		if(checkVGA(c1, c2, c3)){
+			XUartLite_SendByte(0x84000000, space);
+			unsigned char color = XUartLite_RecvByte(0x84000000);
+			XUartLite_SendByte(0x84000000, color);
 		}
 
 		XUartLite_SendByte(0x84000000, NewLine);
@@ -34,7 +49,17 @@ int main(void){
 	return 0;
 }
 
-//this function is to be similar to the nibble to byte
+unsigned char getLEDVal(){
+	unsigned char upper, lower;
+	upper = XUartLite_RecvByte(0x84000000);
+	XUartLite_SendByte(0x84000000, upper);
+	lower = XUartLite_RecvByte(0x84000000);
+	XUartLite_SendByte(0x84000000, lower);
+	upper = outputUpperled(upper);
+	lower = outputLowerled(lower);
+	return upper & lower;
+}
+
 unsigned char outputUpperled(unsigned char c){
 	unsigned char out;
 	if(c == '0'){
@@ -142,6 +167,7 @@ unsigned char outputLowerled(unsigned char c){
 
 	return out;
 }
+
 int checkLED(unsigned char c1, unsigned char c2, unsigned char c3){
 	if((c1 == 'l') && (c2 == 'e') && (c3 == 'd')){
 		return 1;
@@ -151,13 +177,151 @@ int checkLED(unsigned char c1, unsigned char c2, unsigned char c3){
 	}
 }
 
-unsigned char getLEDVal(){
-	unsigned char upper, lower;
-	upper = XUartLite_RecvByte(0x84000000);
-	XUartLite_SendByte(0x84000000, upper);
-	lower = XUartLite_RecvByte(0x84000000);
-	XUartLite_SendByte(0x84000000, lower);
-	upper = outputUpperled(upper);
-	lower = outputLowerled(lower);
-	return upper & lower;
+int checkSWT(unsigned char c1, unsigned char c2, unsigned char c3){
+	if((c1 == 's') && (c2 == 'w') && (c3 == 't')){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+
+int checkVGA(unsigned char c1, unsigned char c2, unsigned char c3){
+	if((c1 == 'v') && (c2 == 'g') && (c3 == 'a')){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+unsigned char getTopNibble(unsigned char ascii){
+	unsigned char top;
+	top = ascii & 0b11110000;
+	return top;
+}
+
+unsigned char getBtmNibble(unsigned char ascii){
+	unsigned char btm;
+	btm = ascii & 0b00001111;
+	return btm;
+}
+
+void outputSWT(unsigned char ascii){
+	unsigned char top, btm;
+	top = getTopNibble(ascii);
+	btm = getBtmNibble(ascii);
+	top = topNibbleToAscii(top);
+	btm = btmNibbleToAscii(btm);
+	XUartLite_SendByte(0x84000000, top);
+	XUartLite_SendByte(0x84000000, btm);
+}
+
+unsigned char topNibbleToAscii(unsigned char nibble){
+	unsigned char out;
+		if(nibble == 0b00000000){
+			out = '0';
+		}
+		else if(nibble == 0b00010000){
+			out = '1';
+		}
+		else if(nibble == 0b00100000){
+			out = '2';
+		}
+		else if(nibble == 0b00110000){
+			out = '3';
+		}
+		else if(nibble == 0b01000000){
+			out = '4';
+		}
+		else if(nibble == 0b01010000){
+			out = '5';
+		}
+		else if(nibble == 0b01100000){
+			out = '6';
+		}
+		else if(nibble == 0b01110000){
+			out = '7';
+		}
+		else if(nibble == 0b10000000){
+			out = '8';
+		}
+		else if(nibble == 0b10010000){
+			out = '9';
+		}
+		else if(nibble == 0b10100000){
+			out = 'A';
+		}
+		else if(nibble == 0b10110000){
+			out = 'B';
+		}
+		else if(nibble == 0b11000000){
+			out = 'C';
+		}
+		else if(nibble == 0b11010000){
+			out = 'D';
+		}
+		else if(nibble == 0b11100000){
+			out = 'E';
+		}
+		else{
+			out = 'F';
+		}
+
+		return out;
+}
+
+unsigned char btmNibbleToAscii(unsigned char nibble){
+	unsigned char out;
+		if(nibble == 0b00000000){
+			out = '0';
+		}
+		else if(nibble == 0b00000001){
+			out = '1';
+		}
+		else if(nibble== 0b00000010){
+			out = '2';
+		}
+		else if(nibble == 0b00000011){
+			out = '3';
+		}
+		else if(nibble == 0b00000100){
+			out = '4';
+		}
+		else if(nibble == 0b00000101){
+			out = '5';
+		}
+		else if(nibble == 0b00000110){
+			out = '6';
+		}
+		else if(nibble == 0b00000111){
+			out = '7';
+		}
+		else if(nibble == 0b00001000){
+			out = '8';
+		}
+		else if(nibble == 0b00001001){
+			out = '9';
+		}
+		else if(nibble == 0b00001010){
+			out = 'A';
+		}
+		else if(nibble == 0b00001011){
+			out = 'B';
+		}
+		else if(nibble == 0b00001100){
+			out = 'C';
+		}
+		else if(nibble == 0b00001101){
+			out = 'D';
+		}
+		else if(nibble == 0b00001110){
+			out = 'E';
+		}
+		else{
+			out = 'F';
+		}
+
+		return out;
 }
